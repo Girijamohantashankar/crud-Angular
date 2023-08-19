13b58fc4ac91ff20ed620d25f5fed7292c043188
Girija <Girija@.com>
2023-08-19 11:24:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,55 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'crud-app';
+  newTask = {
+    title: '',
+    course: '',
+    dueDate: '',
+    fullmark: '',
+    obtainmark: '',
+    editing: false,
+  };
+
+  tasks: any[] = [];
+
+  addTodo() {
+    if (this.newTask.title.trim() !== '') {
+      const newTask = { ...this.newTask };
+      this.tasks.push(newTask);
+      this.resetNewTask();
+    }
+  }
+
+  editTask(index: number) {
+    this.tasks[index].editing = true;
+  }
+
+  saveTask(index: number) {
+    this.tasks[index].editing = false;
+  }
+
+  cancelEdit(index: number) {
+    this.tasks[index].editing = false;
+  }
+
+  deleteTask(index: number) {
+    if (confirm('Are you sure you want to delete this task?')) {
+      this.tasks.splice(index, 1);
+    }
+  }
+
+  private resetNewTask() {
+    this.newTask = {
+      title: '',
+      course: '',
+      dueDate: '',
+      fullmark: '',
+      obtainmark: '',
+      editing: false,
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+   
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
